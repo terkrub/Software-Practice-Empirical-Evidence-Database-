59d95e8f5bc474a6308d558f2ea6535ba7180389
terkrub <terkrub9@gmail.com>
2023-10-01 00:04:36 +1300

    submit Article form

---
+++ frontend/pages/submit.js
@@ -0,0 +1,64 @@
+// pages/submit.js
+import { useState } from 'react';
+import styles from './styles/submitStyles.module.css';
+
+const SubmitArticle = () => {
+  const [title, setTitle] = useState('');
+  const [authors, setAuthors] = useState('');
+  const [journal, setJournal] = useState('');
+  const [year, setYear] = useState('');
+  const [volume, setVolume] = useState('');
+  const [number, setNumber] = useState('');
+  const [pages, setPages] = useState('');
+  const [doi, setDoi] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Here you can handle the submission, e.g., send the data to an API.
+    const articleData = { title, authors, journal, year, volume, number, pages, doi };
+    console.log('Article Data:', articleData);
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1>Submit an Article</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Title:
+          <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required />
+        </label>
+        <label>
+          Authors:
+          <input type="text" value={authors} onChange={(e) => setAuthors(e.target.value)} required />
+        </label>
+        <label>
+          Journal Name:
+          <input type="text" value={journal} onChange={(e) => setJournal(e.target.value)} required />
+        </label>
+        <label>
+          Year of Publication:
+          <input type="number" value={year} onChange={(e) => setYear(e.target.value)} required />
+        </label>
+        <label>
+          Volume:
+          <input type="text" value={volume} onChange={(e) => setVolume(e.target.value)} required />
+        </label>
+        <label>
+          Number:
+          <input type="text" value={number} onChange={(e) => setNumber(e.target.value)} required />
+        </label>
+        <label>
+          Pages:
+          <input type="text" value={pages} onChange={(e) => setPages(e.target.value)} required />
+        </label>
+        <label>
+          DOI:
+          <input type="text" value={doi} onChange={(e) => setDoi(e.target.value)} required />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SubmitArticle;
