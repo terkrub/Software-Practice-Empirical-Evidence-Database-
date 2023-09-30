c622ca098c4c6bdb99680bce82b8990b64bf9179
terkrub <terkrub9@gmail.com>
2023-10-01 01:32:23 +1300

    Update myapi.controller.ts

--- backend/src/myapi/myapi.controller.ts
+++ backend/src/myapi/myapi.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Post, Body } from '@nestjs/common';
 
 @Controller('myapi')
 export class MyapiController {
-    @Post()
+    @Post('test')
     create(@Body() body: any): string {
         const name = body.name;
         return `Received name: ${name}`;
