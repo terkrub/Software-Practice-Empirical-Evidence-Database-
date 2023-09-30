8b4ab54bec9332725607ae047033e391af1aeb15
terkrub <terkrub9@gmail.com>
2023-10-01 01:11:52 +1300

    fix bug

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
 import { AppModule } from './app.module';
 
-const server = express();
-
-async function createNestApp() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  await app.init();
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
-
-createNestApp();
-
-export = server;
+bootstrap();
