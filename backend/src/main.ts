d9bdfd592eec32588ffca6614bd1ab87481a93d5
terkrub <terkrub9@gmail.com>
2023-10-01 01:01:51 +1300

    edit main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+const server = express();
+
+async function createNestApp() {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  await app.init();
 }
-bootstrap();
+
+createNestApp();
+
+export = server;
