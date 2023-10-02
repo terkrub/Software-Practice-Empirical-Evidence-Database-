53e6eef110ac027d0d2d127fc3510fa08b1c7999
terkrub <terkrub9@gmail.com>
2023-10-02 18:40:40 +1300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // or [ 'http://localhost:3000', ...otherOrigins ]
-    credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Accept',
+    origin: true, // Reflect the requesting origin, as defined by the browser
+    credentials: true, // Enable browsers to include credentials in the request
   });
   await app.listen(3000);
 }
