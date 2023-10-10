bca0cf8e1f53e0a603ec5b2e039a1700a017045d
KittipakS <104745274+KittipakS@users.noreply.github.com>
2023-10-10 17:09:17 +1300

    update css

--- frontend/pages/submit.js
+++ frontend/pages/submit.js
@@ -16,6 +16,9 @@ const SubmitArticle = () => {
     isDuplicate: false,
   });
 
+  const [feedback, setFeedback] = useState(''); // To provide feedback to the user
+  const [underReview, setUnderReview] = useState(false); // To simulate the review mechanism
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setArticleData((prevData) => ({ ...prevData, [name]: value }));
@@ -23,33 +26,37 @@ const SubmitArticle = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(articleData);
     try {
-      const res = await axios.post(`http://localhost:4000/articles`,{article: articleData});
-      console.log(res)
+      const res = await axios.post(`http://localhost:4000/articles`, { article: articleData });
+      if (res.status === 200) {
+        setFeedback({ message: 'Article submitted successfully and is under review.', type: 'success' });
+        setUnderReview(true);
+      } else {
+        setFeedback({ message: 'There was an issue submitting the article. Please try again.', type: 'error' });
+      }
     } catch (err) {
-      console.log(err);
+      setFeedback({ message: `Error: ${err.message}`, type: 'error' });
     }
-
-  };
+};
 
   const currentYear = new Date().getFullYear();
   const startYear = currentYear - 100;
 
-  const renderInputField = (label, type, name) => (
+  const renderInputField = (label, type, name, numericOnly = false) => (
     <label className={styles.label} htmlFor={name}>
-      {label}
-      <input
-        type={type}
-        id={name}
-        name={name}
-        value={articleData[name]}
-        onChange={handleChange}
-        required
-        className={styles.input}
-      />
+        {label}
+        <input
+            type={type}
+            id={name}
+            name={name}
+            value={articleData[name]}
+            onChange={handleChange}
+            onKeyPress={numericOnly ? handleNumericInput : null}
+            required
+            className={styles.input}
+        />
     </label>
-  );
+);
 
   const renderSelectField = (label, name) => (
     <label className={styles.label} htmlFor={name}>
@@ -72,26 +79,43 @@ const SubmitArticle = () => {
     </label>
   );
 
-  return (
-    <>
+  const handleNumericInput = (e) => {
+    if (!/[0-9]/.test(e.key)) {
+        e.preventDefault();
+    }
+};
+
+return (
+  <>
       <Header />
       <div className={styles.wrapper}>
-        <div className={styles.container}>
-          <h1 className={styles.h1}>Submit an Article</h1>
-          <form onSubmit={handleSubmit} className={styles.form}>
-            {renderInputField('Title:', 'text', 'title')}
-            {renderInputField('Authors:', 'text', 'authors')}
-            {renderInputField('Journal Name:', 'text', 'journal')}
-            {renderSelectField('Year of Publication:', 'year')}
-            {renderInputField('Volume:', 'text', 'volume')}
-            {renderInputField('Number:', 'text', 'number')}
-            {renderInputField('Pages:', 'text', 'pages')}
-            {renderInputField('DOI:', 'text', 'doi')}
-            <button type="submit" className={styles.button}>Submit</button>
-          </form>
-        </div>
+          <div className={styles.container}>
+              <h1 className={styles.h1}>SUBMIT</h1>
+              
+              <form onSubmit={handleSubmit} className={styles.form}>
+                  <div className={styles.row}>
+                      {renderInputField('Title:', 'text', 'title')}
+                      {renderInputField('Authors:', 'text', 'authors')}
+                  </div>
+                  <div className={styles.row}>
+                      {renderInputField('Journal Name:', 'text', 'journalName')}
+                      {renderSelectField('Year of Publication:', 'yearOfPublication')}
+                  </div>
+                  <div className={styles.row}>
+                      {renderInputField('Volume:', 'text', 'volume', true)}
+                      {renderInputField('Number:', 'text', 'number', true)}
+                  </div>
+                  <div className={styles.row}>
+                      {renderInputField('Pages:', 'text', 'pages', true)}
+                      {renderInputField('DOI:', 'text', 'doi')}
+                  </div>
+                  <button type="submit" className={styles.button}>Submit</button>
+              </form>
+              {feedback && <p className={`${styles.feedback} ${styles[feedback.type]}`}>{feedback.message}</p>}
+              {underReview && <p className={styles.review}>Your article is under review.</p>} 
+          </div>
       </div>
-    </>
+  </>
   );
 };
 
